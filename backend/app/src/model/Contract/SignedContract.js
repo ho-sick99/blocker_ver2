9d1c87785a72ff70e92bdb9356dc309ab1f94554
ho-sick99 <83945722+JunoJuno99@users.noreply.github.com>
2022-10-16 07:57:05 +0900

    체결 계약서 수정, 삭제 API 제작

--- backend/app/src/model/Contract/SignedContract.js
+++ backend/app/src/model/Contract/SignedContract.js
@@ -4,25 +4,58 @@ const N_SignedContractStorage = require("./N_SignedContractStorage");
 const SignedContractStorage = require("./SignedContractStorage");
 
 class SignedContract {
-  async cancle_progress_contract() {
+  // 서명 기입 상태 확인 메서드
+  async check_signed_status() {
+    const contractors = JSON.parse(
+      (await SignedContractStorage.get_contractors(contractData.contract_id))
+        .contractors
+    ); // 현재 해당 계약서의 계약자들 로드
+    const signed = JSON.parse(
+      (await SignedContractStorage.get_check_sign(contractData.contract_id))
+        .signed
+    ); // 현재 해당 계약서의 사인여부 로드
+
+    // 계약자들의 서명이 모두 기입되지 않은 경우
+    if (!signed || contractors.length != signed.length) {
+      return {
+        success: false,
+        msg: "모든 계약자들의 동의가 이루어지지 않았습니다.",
+      }; // 오류 반환
+    }
+
+    return { success: true };
+  }
+
+  // 계약 파기 메서드
+  async delete_contract() {
     const contractData = this.body;
     try {
-      // sign 체크
-      const contractors = JSON.parse(
-        (await SignedContractStorage.get_contractors(contractData.contract_id))
-          .contractors
-      ); // 현재 해당 계약서의 계약자들 로드
-      const signed = JSON.parse(
-        (await SignedContractStorage.get_check_sign(contractData.contract_id))
-          .signed
-      ); // 현재 해당 계약서의 사인여부 로드
+      const signed_status = await check_signed_status(); // DB의 서명 여부 확인
 
-      if (!signed || contractors.length != signed.length) {
-        // 계약자들의 서명이 모두 기입되지 않은 경우
-        return {
-          success: false,
-          msg: "모든 계약자들의 동의가 이루어지지 않았습니다.",
-        }; // 오류 반환
+      // 서명이 모두 기입되지 않은 경우
+      if (!signed_status.success) {
+        return signed_status;
+      }
+
+      const result = await SignedContractStorage.delete_contract(
+        contractData.contract_id
+      ); // 체결 계약서 삭제
+
+      return result;
+    } catch (err) {
+      return { success: false, err };
+    }
+  }
+
+  // 계약 수정 메서드 (체결 -> 미체결)
+  async cancle_contract() {
+    const contractData = this.body;
+    try {
+      const signed_status = await check_signed_status(); // DB의 서명 여부 확인
+
+      // 서명이 모두 기입되지 않은 경우
+      if (!signed_status.success) {
+        return signed_status;
       }
 
       const signedContractData = await SignedContractStorage.view_contract(
@@ -42,6 +75,52 @@ class SignedContract {
       }
     } catch (err) {
       return { success: false, err };
+    }
+  }
+
+  // 서명 기입 메서드
+  async check_sign() {
+    // 현재 서명 정보를 get한다.
+    // 이미 해당 id에 대한 서명이 완료되었다면, (sign includes id) 이미 체크되었다는 오류 반환
+    // 해당 id에 대한 서명이 완료되지 않았다면, sign에 해당 id를 추가한다.
+    try {
+      const contractData = this.body; // contract_id, id 포함
+      const contractors = JSON.parse(
+        (await SignedContractStorage.get_contractors(contractData.contract_id))
+          .contractors
+      ); // 현재 해당 계약서의 계약자들 로드
+      const signed = JSON.parse(
+        (await SignedContractStorage.get_check_sign(contractData.contract_id))
+          .signed
+      ); // 현재 해당 계약서의 사인여부 로드
+
+      if (contractors.id.includes(contractData.id)) {
+        // 현재 client의 id가 contractors에 존재한다면
+        let newSignedId = signed; // 새롭게 반환할 사인 배열
+        if (!newSignedId) {
+          // 초기상태 -> 사인여부 null
+          newSignedId = [contractData.id]; // 현재 계약자 id 추가
+        } else {
+          // 누군가의 사인여부가 존재
+          if (!newSignedId.includes(contractData.id)) {
+            // 현재 사인여부에 해당 계약자의 id가 없다면
+            newSignedId.push(contractData.id); // 계약자 id 추가
+          } else {
+            // 현재 사인여부에 해당 계약자의 id가 존재한다면
+            return { success: false, msg: "이미 서명하셨습니다." };
+          }
+        }
+        const result = await SignedContractStorage.set_check_sign(
+          contractData.contract_id,
+          JSON.stringify(newSignedId)
+        ); // 해당 계약서의 사인여부 수정
+        return result;
+      } else {
+        // 현재 client의 id가 contractors에 존재하지 않는다면
+        return { success: false, msg: "해당 계약의 계약자가 아닙니다." };
+      }
+    } catch (err) {
+      return { success: false, msg: err };
     }
   }
 }
